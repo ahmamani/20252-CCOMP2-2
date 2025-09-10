e8b306579baa3b5b3367cdc1bc6c3860ab1b6ea5
DOCENTE-B302 <DOCENTE-B302@UCSP.AULAB>
2025-09-10 08:24:42 -0500

    funciones

--- semana4/nroPalindromo/main.cpp
+++ semana4/nroPalindromo/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -12,6 +13,32 @@ int main()
             2133214 -> no es palindromo
             31311313-> es palindromo
     */
+    long n;
+    cout << "Ingrese un numero: ";
+    cin >> n;
+
+    int numC = n;
+    int numDigits = 0;
+    while(numC > 0) {
+        numC /= 10;
+        numDigits++;
+    }
+
+    while( n != 0 ){
+        int ini = n / pow(10, numDigits-1);
+        int fin = n % 10;
+        if(ini != fin){
+            cout << "NO ES PALINDROMO" << endl;
+            break;
+        }
+        n = (n % static_cast<int>(pow(10, numDigits-1))) / 10;
+        numDigits-=2;
+        cout << ini << endl;
+        cout << fin << endl;
+        cout << n << endl;
+    }
+
+
 
     return 0;
 }
