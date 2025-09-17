85b38ceb90e1211bc9dffc661be7a2f0cb3b8b9f
DOCENTE-B302 <DOCENTE-B302@UCSP.AULAB>
2025-09-17 08:26:57 -0500

    paso x referencia

--- semana5/strings/main.cpp
+++ semana5/strings/main.cpp
@@ -4,13 +4,23 @@ using namespace std;
 
 int main()
 {
-    string cad = "Ciencia de la computacion";
+    string cad;// = "Ciencia de la computacion";
 
     /**
     * contar la cantidad de i's que tiene dicha cadena
     */
-    int i = 0;
+    cout << "Ingrese una cadena: ";
+    getline(cin, cad);
+    char car;
+    cout << "Ingrese un caracter: ";
+    cin >> car;
 
-    cout << i << endl;
+    int cont = 0;
+    for(int i = 0; i < cad.length(); i++) {
+        if (cad.at(i) == car)
+            cont++;
+    }
+
+    cout << cont << endl;
     return 0;
 }
